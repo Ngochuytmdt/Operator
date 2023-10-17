a8c990fbe60be8d4e2cbc0cfcfa0b136e9576aad
Ngochuytmdt <146686315+Ngochuytmdt@users.noreply.github.com>
2023-10-17 08:23:10 +0700

    Add files via upload
    s

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,31 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10, b = 5 ;
+        //Phep cong
+        System.out.println(a + b);
+        //Phep tru
+        System.out.println(a - b);
+        //Phep nhan
+        System.out.println(a * b);
+        //Phep chia
+        System.out.println(a / b);
+        
+        boolean c = a > b;
+        System.out.println(c);
+        
+        c = a >= b;
+        System.out.println(c);
+        
+        c = a < b;
+        System.out.println(c);
+        
+        c = a <= b;
+        System.out.println(c);
+        
+	}
+
+}
